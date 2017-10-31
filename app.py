04c7ac0e4fc983afe1b5921febc2db7bedf1ff64
ling.gao <ling.gao@gmail.com>
2017-10-31 16:03:15 -0400

    change socketio

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,flash,redirect,render_template,url_for, json, request,jsonify, session
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, emit
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -7,10 +7,13 @@ app.config["SECRUITY_KEY"] = 'j7I2VDTTedstsl249uigUrtLzK0iRdTt'
 socketio = SocketIO(app)
 mongo = PyMongo(app)
 
-@socketio.on('message')
-def handleMessage(msg):
-    print(msg)
-    send(msg, broadcast=True)
+def messageRecived():
+  print( 'message was received!!!' )
+
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'my response', json, callback=messageRecived )
 
 @app.route("/")
 def index():
@@ -18,4 +21,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug = True )
--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
-	var socket = io.connect('http://0.0.0.0:5000');
-	socket.on('connect', function() {
-		socket.send('User has connected!');
-	});
-	socket.on('message', function(msg) {
-		$("#messages").append('<li>'+msg+'</li>');
-		console.log('Received message');
-	});
-	$('#sendbutton').on('click', function() {
-		socket.send($('#myMessage').val());
-		$('#myMessage').val('');
-	});
-});
+
+var socket = io.connect( 'http://' + document.domain + ':' + location.port )
+     // broadcast a message
+     socket.on( 'connect', function() {
+       socket.emit( 'my event', {
+         data: 'User Connected'
+       } )
+       var form = $( 'form' ).on( 'submit', function( e ) {
+         e.preventDefault()
+         let user_name = $( 'input.username' ).val()
+         let user_input = $( 'input.message' ).val()
+         socket.emit( 'my event', {
+           user_name : user_name,
+           message : user_input
+         } )
+         // empty the input field
+         $( 'input.message' ).val( '' ).focus()
+       } )
+     } )
+     // capture message
+     socket.on( 'my response', function( msg ) {
+       console.log( msg )
+       if( typeof msg.user_name !== 'undefined' ) {
+         $( 'h1' ).remove()
+         $( 'div.message_holder' ).append( '<div class="msg_bbl"><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )
+       }
+     } )
