1e9fdc0c154b1389256a2120f434ee233cc4f806
ling.gao <ling.gao@gmail.com>
2017-10-31 15:49:42 -0400

    ip 127.0.0.1 to 0.0.0.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var socket = io.connect('http://127.0.0.1:5000');
+	var socket = io.connect('http://0.0.0.0:5000');
 	socket.on('connect', function() {
 		socket.send('User has connected!');
 	});
